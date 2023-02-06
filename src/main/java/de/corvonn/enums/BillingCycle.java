b25c0fe9c98e7ed3c426b9d5e1ea5ad68e9ce08e
Only <>
2023-02-06 22:37:07 +0100

    Added months per BillingCycle

--- src/main/java/de/corvonn/enums/BillingCycle.java
+++ src/main/java/de/corvonn/enums/BillingCycle.java
@@ -9,24 +9,26 @@ public enum BillingCycle {
     /**
      * The rented resource must be paid monthly.
      */
-    MONTHLY("Monthly"),
+    MONTHLY("Monthly", 1),
     /**
      * The rented resource must be paid every three months.
      */
-    QUARTERLY("Quarterly"),
+    QUARTERLY("Quarterly", 3),
     /**
      * The rented resource must be paid semi-annually.
      */
-    SEMI_ANNUALLY("Semi-Annually"),
+    SEMI_ANNUALLY("Semi-Annually", 6),
     /**
      * The rented resource must be paid annually.
      */
-    ANNUALLY("Annually");
+    ANNUALLY("Annually", 12);
 
     private final String cycleName;
+    private final int billingCycle;
 
-    BillingCycle(String type) {
+    BillingCycle(String type, int billingCycle) {
         this.cycleName = type;
+        this.billingCycle = billingCycle;
     }
 
     /**
@@ -35,6 +37,15 @@ public enum BillingCycle {
      */
     public String getCycleName() {
         return cycleName;
+    }
+
+    /**
+     * Specifies the number of months in a pay period. "The service must be paid every x months".
+     * @return the months per billingCycle
+     */
+    @SuppressWarnings("unused")
+    public int getMonthsPerBillingCycle() {
+        return billingCycle;
     }
 
     private static final HashMap<String, BillingCycle> typeHashMap = new HashMap<>();
